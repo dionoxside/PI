2ef1bf446a76c4f25b3f42337199ef01edf3145f
novan.diki@gmail.com <novan.diki@gmail.com>
2016-08-09 20:01:29 +0700

    bootstrap

---
+++ config/bank-accounts.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'bca' => [
+        'title' => 'BCA',
+        'bank' => 'BCA',
+        'name' => 'SofaIndo',
+        'number' => '45454545'
+    ],
+    'bni' => [
+        'title' => 'BNI',
+        'bank' => 'BNI',
+        'name' => 'SofaIndo',
+        'number' => '787687678'
+    ],
+    'mandiri' => [
+        'title' => 'Mandiri',
+        'bank' => 'Mandiri',
+        'name' => 'SofaIndo',
+        'number' => '23223232'
+    ],
+    'atm-bersama' => [
+        'title' => 'ATM Bersama',
+        'bank' => 'Mandiri',
+        'name' => 'SofaIndo',
+        'number' => '23223232'
+    ],
+];
